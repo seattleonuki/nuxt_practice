aaac8b9eb9e401a9377fc06e58892af32bd17164
shunsuke <shunsuke.onuki@seattleconsulting.co.jp>
2019-09-11 20:53:28 +0900

    コメント追加

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
     <hr>
     <div>
       <!-- {{ users[0].id }}, {{ users[0].name }} -->
+      <!-- devでいじりました -->
       <ul>
         <li v-for="user in users" :key='user.id'>
           {{ user.id }}, {{ user.name }}
