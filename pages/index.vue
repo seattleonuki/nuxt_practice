35145d2b7c9f287ccfc41bfe24fd8d0ad94ad989
shunsuke <shunsuke.onuki@seattleconsulting.co.jp>
2019-09-11 23:08:11 +0900

    9/11最後のpush

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,6 @@
     
     <hr>
     <router-link to="/price">price page</router-link>
-    <p>subscription</p>
-    <p>aaa</p>
     <hr>
     <div>
       <!-- {{ users[0].id }}, {{ users[0].name }} -->
@@ -18,7 +16,6 @@
     </div>
     <hr>
     <div>
-      <p>shun</p>
       <img src="~/assets/cat.jpg">
     </div>
   </section>
